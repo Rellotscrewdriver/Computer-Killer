139571cc7597a1989c596308d91f8b92ac8014bf
Rellotscrewdriver <JKanimations71@gmail.com>
2021-05-09 19:30:27 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Slow-Down-your-Computer
+# Disclainmer: Your Device would get slowed down if you run the code on the complier!
+it was made for fun project plz don't sue me github :D
+
+You Wonder what might be special about it. When I was developing this program, I never Executed this program nor you must
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    using namespace std; //do I need to explain this?
+
+    //it simply asks the user to enter the filename
+    string text;
+    cout << "Enter Your Filename: ";
+    //I used getline because cin usually terminates with whitespace.
+    getline(cin, text);
+    text += ".txt";
+
+    ofstream virus(text);
+    while(true){
+        virus << "You've been hacked, go cry in corner" << text;
+    }
+
+    //I should closing the file but for this program, nope.
+    //virus.close();
+    return 0;
+}
